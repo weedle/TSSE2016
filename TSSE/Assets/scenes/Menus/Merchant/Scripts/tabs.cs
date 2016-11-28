7412c336b9d7bae9f6875f2c625f21b464bc92bd
Jenne H <iv.castaire@gmail.com>
2016-11-28 11:45:30 -0800

    Merchant menu commit

---
+++ TSSE/Assets/scenes/Menus/Merchant/Scripts/tabs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class tabs : MonoBehaviour {
+	public Button button;
+	public string panelName;
+
+	void Start() {
+		button = gameObject.GetComponent<Button> ();
+		button.onClick.AddListener (changePanes);
+	}
+
+	void changePanes() {
+		var panel = GameObject.Find (panelName);
+		panel.transform.SetAsLastSibling();
+	}
+}
