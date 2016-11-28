d08a4daeefef0a1f3a640d8b8d81540305b0de0c
ZHUALICE <alice-desu@hotmail.com>
2016-11-27 17:43:36 -0800

    Personal Info Menus added

---
+++ TSSE/Assets/scripts/MenuHandling/ButtonManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void SettingsBtn (string Settings)
+    {
+        SceneManager.LoadScene(Settings); 
+    }
+
+    public void BackBtn (string PersonalInfo)
+    {
+        SceneManager.LoadScene(PersonalInfo);
+    }
+
+    public void QuestsBtn (string Quests)
+    {
+        SceneManager.LoadScene(Quests);
+    }
+
+    public void MyItemsBtn (string MyItems)
+    {
+        SceneManager.LoadScene(MyItems); 
+    }
+
+    public void ShipsBtn (string Ships)
+    {
+        SceneManager.LoadScene(Ships);
+    }
+
+    public void ShipsBtnLoad(string Ships)
+    {
+        SceneManager.LoadScene("Ships");
+    }
+    // switch case 
+}
