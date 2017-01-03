d8893cf528c8f553c881bc56eaf373b65bcf2bdf
Kevin Jayamanna <mercury0110111@gmail.com>
2017-01-03 11:28:33 -0800

    Abstracted character info out

--- TSSE/Assets/scenes/dialogueFail/Character.cs
+++ TSSE/Assets/scenes/dialogueFail/Character.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+
+[XmlRoot("DialogueCollection")]
 public abstract class Character
 {
-    /*
-    public class Dialogue
+    public struct Dialogue
     {
+        [XmlAttribute("DialogueRegular")]
         public string dialogueRegular;
+        [XmlAttribute("DialogueYes")]
         public string dialogueYes;
+        [XmlAttribute("DialogueNo")]
         public string dialogueNo;
     };
-    */
+
+    [XmlArray("Dialogues")]
+    [XmlArrayItem("Dialogue")]
+    public List<Dialogue> dialogues = new List<Dialogue>();
+
     //public string Name;
     //private string factionRaw;
     //public ShipDefinitions.Faction Affiliation;
@@ -17,12 +27,19 @@ public abstract class Character
     //public int level;
     //public int dialogueLevel;
 
-
     public abstract void initialize();
 
-    public abstract string getRegularDialogue();
+    public string getRegularDialogue()
+    {
+        return dialogues[0].dialogueRegular;
+    }
 
-    public abstract string getYesDialogue();
-
-    public abstract string getNoDialogue();
+    public string getYesDialogue()
+    {
+        return dialogues[0].dialogueYes;
+    }
+    public string getNoDialogue()
+    {
+        return dialogues[0].dialogueNo;
+    }
 }
